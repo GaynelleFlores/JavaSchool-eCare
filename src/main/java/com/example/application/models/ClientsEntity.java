052b88683f9abec1e248bb4834383a116f4f1b3c
GaynelleFlores <gflores@student.21-school.ru>
2022-03-02 13:14:37 +0300

    fixed lombok

--- src/main/java/com/example/application/models/ClientsEntity.java
+++ src/main/java/com/example/application/models/ClientsEntity.java
@@ -1,88 +1,44 @@
 package com.example.application.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
+@Data
 @Table(name = "clients", schema = "public", catalog = "postgres")
 public class ClientsEntity {
 
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     @Column(name = "id")
-    @Getter
-    @Setter
     private int id;
 
     @Column(name = "name")
-    @Getter
-    @Setter
     private String name;
 
     @Temporal(TemporalType.DATE)
     @Column(name = "birthdate")
-    @Getter
-    @Setter
     private Date birthdate;
 
     @Column(name = "surname")
-    @Getter
-    @Setter
     private String surname;
 
     @Column(name = "passport")
-    @Getter
-    @Setter
     private String passport;
 
     @Column(name = "address")
-    @Getter
-    @Setter
     private String address;
 
     @Column(name = "email")
-    @Getter
-    @Setter
     private String email;
 
     @Column(name = "password")
-    @Getter
-    @Setter
     private String password;
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "client", cascade = CascadeType.PERSIST)
-    @Getter
-    @Setter
+    @EqualsAndHashCode.Exclude
     private List<ContractsEntity> contracts;
-
-    @Override
-    public String toString() {
-        return "ClientsEntity[" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", birthdate=" + birthdate +
-                ", surname='" + surname + '\'' +
-                ", passport='" + passport + '\'' +
-                ", address='" + address + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password +
-                ']';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ClientsEntity that = (ClientsEntity) o;
-        return id == that.id && name.equals(that.name) && birthdate.equals(that.birthdate) && surname.equals(that.surname) && passport.equals(that.passport) && address.equals(that.address) && email.equals(that.email) && password.equals(that.password) && contracts.equals(that.contracts);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, birthdate, surname, passport, address, email, password, contracts);
-    }
 }
--- src/main/java/com/example/application/models/OptionsEntity.java
+++ src/main/java/com/example/application/models/OptionsEntity.java
@@ -1,68 +1,34 @@
 package com.example.application.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import javax.persistence.*;
 import java.math.BigInteger;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
+@Data
 @Table(name = "options", schema = "public", catalog = "postgres")
 public class OptionsEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "id")
-    @Getter
-    @Setter
     private int id;
 
     @Column(name = "title")
-    @Getter
-    @Setter
     private String title;
 
     @Column(name = "price")
-    @Getter
-    @Setter
     private BigInteger price;
 
     @Column(name = "connection_cost")
-    @Getter
-    @Setter
     private BigInteger connection_cost;
 
-
     @ManyToMany(mappedBy = "options")
-    @Getter
-    @Setter
+    @EqualsAndHashCode.Exclude
     Set<ContractsEntity> contracts;
 
     @ManyToMany(mappedBy = "allowed_options")
-    @Getter
-    @Setter
+    @EqualsAndHashCode.Exclude
     Set<PlansEntity> plans;
-
-    @Override
-    public String toString() {
-        return "OptionsEntity[" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", price=" + price +
-                ", connection_cost=" + connection_cost +
-                ']';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        OptionsEntity that = (OptionsEntity) o;
-        return id == that.id && title.equals(that.title) && price.equals(that.price) && connection_cost.equals(that.connection_cost);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, price, connection_cost);
-    }
 }
