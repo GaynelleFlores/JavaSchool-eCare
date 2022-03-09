2e0e531e65aadc0a90f7b7e3834c59cb75d799c0
GaynelleFlores <gflores@student.21-school.ru>
2022-03-09 13:39:39 +0300

    fix config files

--- src/main/java/com/example/application/config/EntityManagerConfig.java
+++ src/main/java/com/example/application/config/EntityManagerConfig.java
@@ -1,16 +1,13 @@
 package com.example.application.config;
 
-import org.dozer.DozerBeanMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 
 @Configuration
-public class AutowiredConfig {
+public class EntityManagerConfig {
 
     @Bean
     public static EntityManagerFactory getEntityManagerFactory() {
@@ -20,10 +17,5 @@ public class AutowiredConfig {
     @Bean
     public static EntityManager getEntityManager(EntityManagerFactory entityManagerFactory) {
         return entityManagerFactory.createEntityManager();
-    }
-    
-    @Bean
-    public DozerBeanMapper beanMapper() {
-        return new DozerBeanMapper();
     }
 }
---
+++ src/main/java/com/example/application/config/MappingConfig.java
@@ -0,0 +1,13 @@
+package com.example.application.config;
+
+import org.dozer.DozerBeanMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MappingConfig {
+    @Bean
+    public DozerBeanMapper beanMapper() {
+        return new DozerBeanMapper();
+    }
+}
