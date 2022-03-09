e67ff877d4d8d7a84b691fff5b7b11f4e1d8a4a9
GaynelleFlores <gflores@student.21-school.ru>
2022-03-09 12:32:04 +0300

    typo

--- src/main/java/com/example/application/config/AutowiredConfig.java
+++ src/main/java/com/example/application/config/AutowiredConfig.java
@@ -1,6 +1,5 @@
 package com.example.application.config;
 
-
 import org.dozer.DozerBeanMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
